9f1a6ab508c2e501cdb965ddd44a314c89e521ee
JamieThanh <thanhjamieai@gmail.com>
2021-12-29 14:07:31 +0700

    35

--- LearnJS/app.js
+++ LearnJS/app.js
@@ -1,9 +1,27 @@
-var languages = [1, 2, 3, 4];
+// var languages = [1, 2, 3, 4];
 
-// languages.
-if(languages > 3 ){
-    languages.pop();
-}else{
-    languages.splice(-2,2)
-}
-console.log(languages);
+// // languages.
+// if(languages > 3 ){
+//     languages.pop();
+// }else{
+//     languages.splice(-2,2)
+// }
+// console.log(languages);
+
+// var a = 1;
+// var b = 1;
+// console.log(typeof a,b);
+
+// function calculateTriangleArea(a, b) {
+//     if (a && b > 0) {
+//         return (a * b) / 2;
+//     } else {
+//         return false;
+//     }
+// }
+
+// calculateTriangleArea(2, 8);
+
+var a = "123"
+var b = "222"
+b.includes
