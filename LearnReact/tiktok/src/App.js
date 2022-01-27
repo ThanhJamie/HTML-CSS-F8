1b485d1eb58781a90b63e7016c56aa7bff988e63
JamieThanh <thanhjamieai@gmail.com>
2022-01-27 16:25:21 +0700

    .

--- LearnReact/tiktok/src/App.js
+++ LearnReact/tiktok/src/App.js
@@ -1,28 +1,23 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-const gifts = [
-	'CPU i9',
-	'RAM 32GB RGB',
-	'RGB Keyboard',
-]
+// const gifts = [
+// 	'CPU i9',
+// 	'RAM 32GB RGB',
+// 	'RGB Keyboard',
+// ]
 
 function App() {
-	const [gift, setGift] = useState()
-
-	const randomGift = () => {
-		const index = Math.floor(Math.random() * gifts.length)
-
-		setGift(gifts[index])
-	}
-
-	return (
-		<div className="App" style={{ padding: 20 }}>
-			<h1>{gift || 'Chưa có phần thưởng'}</h1>
-			<button onClick={randomGift}>Lấy thưởng</button>
-		</div>
-
-	);
-
+    const [name, setName] = useState("");
+    console.log(name);
+    return (
+        <div style={{ padding: 20 }}>
+            <input
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            ></input>
+            <button onClick={() => setName("Dang Chi Thanh")}>Change</button>
+        </div>
+    );
 }
 
 export default App;
