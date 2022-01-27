eb19ebabfec6da8914dbb2c4ff788ca63f66eedc
JamieThanh <thanhjamieai@gmail.com>
2022-01-27 16:58:31 +0700

    checkbox

--- LearnReact/tiktok/src/App.js
+++ LearnReact/tiktok/src/App.js
@@ -7,49 +7,46 @@ import { useState } from "react";
 // ]
 
 const courses = [
-    {
-        id: 1,
-        name: "Hoc HTML,CSS",
-    },
-    {
-        id: 2,
-        name: "Hoc JavaScript cơ bản",
-    },
-    {
-        id: 3,
-        name: "Hoc JavaScript cơ bản",
-    },
-    {
-        id: 4,
-        name: "Hoc React",
-    },
+	{
+		id: 1,
+		name: "Hoc HTML,CSS",
+	},
+	{
+		id: 2,
+		name: "Hoc JavaScript cơ bản",
+	},
+	{
+		id: 3,
+		name: "Hoc JavaScript cơ bản",
+	},
+	{
+		id: 4,
+		name: "Hoc React",
+	},
 ];
 
 function App() {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
+	const [checked, setChecked] = useState(2);
 
-    const handleSubmit = () => {
-        console.log({
-            name,
-            email,
-        });
-    };
+	console.log(checked);
+	const handleSubmit = () => {};
 
-    // console.log(name);
-    return (
-        <div style={{ padding: 32 }}>
-            <input
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-            ></input>
-            <input
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-            ></input>
-            <button onClick={handleSubmit}>SUBMIT</button>
-        </div>
-    );
+	// console.log(name);
+	return (
+		<div style={{ padding: 32 }}>
+			{courses.map((course) => (
+				<div key={course.id}>
+					<input
+						type="radio"
+						checked = {checked === course.id}
+						onChange={() => setChecked(course.id)}
+					/>
+					{course.name}
+				</div>
+			))}
+			<button onClick={handleSubmit}>SUBMIT</button>
+		</div>
+	);
 }
 
 export default App;
