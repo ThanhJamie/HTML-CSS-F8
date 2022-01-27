766eb06135b39f0fdd8b0c3f921e62c8e480352e
JamieThanh <thanhjamieai@gmail.com>
2022-01-28 01:55:43 +0700

    .

--- LearnReact/tiktok/src/App.js
+++ LearnReact/tiktok/src/App.js
@@ -1,35 +1,13 @@
 import { useState } from "react";
+import Content from "./Content";
 
 function App() {
-    const storageJobs = JSON.parse(localStorage.getItem('jobs'))
-    const [job, setJob] = useState("");
-    const [jobs, setJobs] = useState(storageJobs ?? []);
-
-
-    const handleSubmit = () => {
-        setJobs((prev) => {
-            const newJobs = [...prev, job];
-
-            // Lưu trong LOCALSTORAGE
-            const jsonJobs = JSON.stringify(newJobs);
-            localStorage.setItem("jobs", jsonJobs);
-            return newJobs;
-        });
-        setJob("");
-    };
+    const [show, setShow] = useState(false);
 
     return (
         <div style={{ padding: 32 }}>
-            <input value={job} onChange={(e) => setJob(e.target.value)} />
-
-            <button onClick={handleSubmit} style={{ margin: 20 }}>
-                Add
-            </button>
-            <ul>
-                {jobs.map((job, index) => (
-                    <li key={index}>{job}</li>
-                ))}
-            </ul>
+            <button onClick={() => setShow(!show)}>Toggle</button>
+            {show && <Content />}
         </div>
     );
 }
