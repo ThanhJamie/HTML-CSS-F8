9a7d0855b65fa474728aa8572f35dbbfa56d0ad1
JamieThanh <thanhjamieai@gmail.com>
2022-01-27 22:09:40 +0700

    27

--- LearnReact/tiktok/src/App.js
+++ LearnReact/tiktok/src/App.js
@@ -1,65 +1,35 @@
 import { useState } from "react";
 
-// const gifts = [
-// 	'CPU i9',
-// 	'RAM 32GB RGB',
-// 	'RGB Keyboard',
-// ]
-
-const courses = [
-    {
-        id: 1,
-        name: "Hoc HTML,CSS",
-    },
-    {
-        id: 2,
-        name: "Hoc JavaScript cơ bản",
-    },
-    {
-        id: 3,
-        name: "Hoc JavaScript cơ bản",
-    },
-    {
-        id: 4,
-        name: "Hoc React",
-    },
-];
-
 function App() {
-    const [checked, setChecked] = useState([]);
+    const storageJobs = JSON.parse(localStorage.getItem('jobs'))
+    const [job, setJob] = useState("");
+    const [jobs, setJobs] = useState(storageJobs ?? []);
 
-    console.log(checked);
-
-    const handleCheck = (id) => {
-        setChecked((prev) => {
-            const isChecked = checked.includes(id);
-            if (isChecked) {
-                return checked.filter((item) => item !== id);
-            } else {
-                return [...prev, id];
-            }
-        });
-    };
 
     const handleSubmit = () => {
-        console.log({
-            ids: checked,
+        setJobs((prev) => {
+            const newJobs = [...prev, job];
+
+            // Lưu trong LOCALSTORAGE
+            const jsonJobs = JSON.stringify(newJobs);
+            localStorage.setItem("jobs", jsonJobs);
+            return newJobs;
         });
+        setJob("");
     };
 
     return (
         <div style={{ padding: 32 }}>
-            {courses.map((course) => (
-                <div key={course.id}>
-                    <input
-                        type="checkbox"
-                        checked={checked.includes(course.id)}
-                        onChange={() => handleCheck(course.id)}
-                    />
-                    {course.name}
-                </div>
-            ))}
-            <button onClick={handleSubmit}>SUBMIT</button>
+            <input value={job} onChange={(e) => setJob(e.target.value)} />
+
+            <button onClick={handleSubmit} style={{ margin: 20 }}>
+                Add
+            </button>
+            <ul>
+                {jobs.map((job, index) => (
+                    <li key={index}>{job}</li>
+                ))}
+            </ul>
         </div>
     );
 }
