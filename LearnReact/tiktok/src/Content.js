5a7c64875745db8092c03413bdcf3dcb650e7f98
JamieThanh <thanhjamieai@gmail.com>
2022-01-28 02:04:03 +0700

    .

--- LearnReact/tiktok/src/Content.js
+++ LearnReact/tiktok/src/Content.js
@@ -1,79 +1,25 @@
 import { useEffect, useState } from "react";
 
-const tabs = ['posts', 'comments', 'albums']
 
 function Content() {
-    // const [title, setTitle] = useState("");
-    const [posts, setPosts] = useState([]);
-    const [type, setType] = useState("posts");
-    const [showGoToTop, setShowGoToTop] = useState(false);
-
-    // console.log(type);
-
-
+    const [width, setWidth] = useState(window.innerWidth)
+    
     useEffect(() => {
-        // console.log('Mounted');
-        fetch(`https://jsonplaceholder.typicode.com/${type}`)
-            .then(res => res.json())
-            .then(posts => {
-                setPosts(posts);
-            })
-
-    }, [type]);
-
-    useEffect(() => {
-        const handleScroll = () => {
-            if (window.scrollY >= 200) {
-                // Show
-                setShowGoToTop(true)
-            } else {
-                setShowGoToTop(false)
-            }
+        
+        const handleResize = () => {
+            setWidth(window.innerWidth);
         }
-        window.addEventListener('scroll', handleScroll)
-        // Cleanup Funtion
+        
+        window.addEventListener('resize',handleResize)
+
         return () => {
-            window.removeEventListener('scroll', handleScroll)
+            window.removeEventListener('resize',handleResize)
         }
-
-    }, [])
+    },[])
 
     return (
         <div>
-            {tabs.map(tab => (
-                <button
-                    key={tab}
-                    style={type === tab ? {
-                        color: '#fff',
-                        backgroundColor: '#333'
-                    } : {}}
-                    onClick={() => setType(tab)}
-                >
-                    {tab}
-                </button>
-            ))}
-
-            {/* <input
-                value={title}
-                onChange={e => setTitle(e.target.value)}
-                /> */}
-
-
-            {posts.map(post => (
-                <li key={post.id}>{post.title || post.email}</li>
-            ))}
-
-            {showGoToTop && (
-                <button
-                    style={{
-                        position: 'fixed',
-                        right: 20,
-                        bottom: 20,
-                    }}
-                >
-                    Go to Top
-                </button>
-            )}
+            <h1>{width}</h1>
         </div>
     );
 }
