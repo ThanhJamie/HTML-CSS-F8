2b467ba3155ef9ac89b56c2d0875940425712753
JamieThanh <thanhjamieai@gmail.com>
2022-02-02 22:20:41 +0700

    .

--- LearnExpressJS/index.js
+++ LearnExpressJS/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const morgan = require('morgan')
+
+app.use(morgan('combined'))
 
 app.get('/', (req, res) => {
-    var a = 1
-    var b = 2
-    var c = a + b
     return res.send('Hello World!')
 })
 
